cc05729b3d590bbe4971e7d1c55fce909afc5b7a
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2020-11-23 17:35:26 +0300

    Added filtering to "kks top"

--- kks/cmd/top.py
+++ kks/cmd/top.py
@@ -5,9 +5,20 @@ from kks.util import get_valid_session, load_links
 
 
 @click.command()
-def top():
-    """Parse and display user standings"""
+@click.option('-a', '--all', '_all', is_flag=True,
+              help='Print the whole table')
+@click.option('-c', '--contest', type=str,
+              help='Print the results of the selected contest')
+def top(contest, _all):
+    """
+    Parse and display user standings
 
+    \b
+    Example usage:
+        kks top     show only the last contest
+        kks top -a
+        kks top -c sm01
+    """
     session = get_valid_session()
     if session is None:
         return
@@ -17,18 +28,29 @@ def top():
         click.secho('Auth data is invalid, use "kks auth" to authorize', fg='red', err=True)
         return
 
-    standings = ejudge_standings(links, session)
+    if _all:
+        contest = '_all_'
+    elif contest is None:
+        contest = '_last_'
+
+    task_map, standings = ejudge_standings(links, session)
+    if task_map is None:
+        click.secho('Standings are not available', fg='red', err=True)
+        return
+    if not task_map.contest_exists(contest):
+        click.secho('This contest doesn\'t exist', fg='red', err=True)
+        return
 
     row_format = "{:>6}  {:25} {:>7} {:>6}  {}"
+    header = row_format.format("Place", "User", "Solved", "Score", "Tasks")
 
-    click.secho(row_format.format("Place", "User", "Solved", "Score", "Tasks"), fg='white', bold=True)
+    click.secho(header, fg='white', bold=True)
 
     for row in standings:
         tasks = ' '.join([
             click.style('{:>3}'.format(task.score or ''), fg=task.color(), bold=task.bold())
-            for task in row.tasks
+            for task in task_map.filter(row.tasks, contest)
         ])
-
         string = row_format.format(row.place, row.user, row.solved, row.score, tasks)
         click.secho(string, fg=row.color(), bold=row.bold())
 
