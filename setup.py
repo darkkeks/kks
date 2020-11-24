d6e983953a14cd466cc5a4771277407a62aedf7b
darkkeks <darkkeks@rambler.ru>
2020-11-24 15:56:27 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='kokos',
     description='KoKoS helper tool',
     author='Vyacheslav Boben',
-    version='1.0.16',
+    version='1.0.17',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
