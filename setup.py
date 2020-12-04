27b7cef444cf333039684f7dd3a63d499e0bc4c9
darkkeks <darkkeks@rambler.ru>
2020-12-05 00:09:22 +0300

    Добавил publish в github actions

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
 setup(
     name='kokos',
     description='KoKoS helper tool',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author='Vyacheslav Boben',
+    url='https://github.com/DarkKeks/kks',
     version='1.2.3',
     packages=find_packages(),
     include_package_data=True,
