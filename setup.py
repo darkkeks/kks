d8b1ce3941e0b55c0d5f096f5bd1c408d738bb52
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2020-12-04 12:07:23 +0300

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='kokos',
     description='KoKoS helper tool',
     author='Vyacheslav Boben',
-    version='1.2.2',
+    version='1.2.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
