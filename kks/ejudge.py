323f98d2db9c29eb216f00dba14f72ea5709a8f4
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2021-04-02 16:49:17 +0300

    add tests for "kks top"

--- kks/ejudge.py
+++ kks/ejudge.py
@@ -1,6 +1,7 @@
 from copy import copy
 from datetime import datetime, timedelta
 from itertools import groupby
+from typing import Optional
 from urllib.parse import quote as urlquote
 
 # import requests  # we use lazy imports to improve load time for local commands
@@ -163,7 +164,7 @@ class Report:
 
 
 class TaskScore:
-    def __init__(self, contest, score, status):
+    def __init__(self, contest: str, score: Optional[str], status: str):
         self.contest = contest
         self.score = score
         self.status = status
@@ -239,7 +240,7 @@ class Deadlines:
 
 class ProblemInfo:
     """Subset of task info table used for max score estimation"""
-    def __init__(self, full_score, run_penalty, current_penalty, deadlines):
+    def __init__(self, full_score: int, run_penalty: int, current_penalty: int, deadlines: Deadlines):
         self.full_score = full_score
         self.run_penalty = run_penalty
         self.current_penalty = current_penalty
@@ -284,7 +285,7 @@ class FullProblem(SummaryProblem):
         if output_title is not None:
             output_data = output_title.find_next('pre').text
 
-        return input_data, output_data 
+        return input_data, output_data
 
     @staticmethod
     def parse_statement(html):
---
+++ tests/test_top_max.py
@@ -0,0 +1,145 @@
+from copy import copy
+from os import environ
+
+import pytest
+
+from kks.ejudge import Deadlines, ProblemInfo, StandingsRow, Status, TaskScore
+from kks.cmd.top import recalc_task_score
+
+class FrozenProblemInfo(ProblemInfo):
+    def __init__(self, past_deadline, full_score, run_penalty, current_penalty):
+        super().__init__(full_score, run_penalty, current_penalty, Deadlines(None, None))
+        self._past_deadline = past_deadline
+
+    def past_deadline(self):
+        return self._past_deadline
+
+
+def contest_name(is_kr):
+    return 'kr123' if is_kr else 'sm123'
+
+
+# ---------- SCORE GENERATORS ----------
+
+
+def score_ok():
+    return TaskScore(contest_name(False), '100', Status.OK)
+
+
+def score_review():
+    return TaskScore(contest_name(False), '100', Status.REVIEW)
+
+
+def score_partial():
+    return TaskScore(contest_name(False), '0', Status.PARTIAL)
+
+
+def score_testing(has_partial=False, is_kr=False):
+    return TaskScore(contest_name(is_kr), '0' if has_partial else None, Status.TESTING)
+
+
+def score_rejected(has_partial=False):
+    return TaskScore(contest_name(False), '0' if has_partial else None, Status.REJECTED)
+
+
+def score_not_submitted():
+    return TaskScore(contest_name(False), None, Status.NOT_SUBMITTED)
+
+
+# ---------- PROBLEM INFO GENERATORS ----------
+
+
+def problem_past_deadline(full_score=100, run_penalty=10, current_penalty=100):
+    return FrozenProblemInfo(True, full_score, run_penalty, current_penalty)
+
+
+def normal_problem(full_score=100, run_penalty=10, current_penalty=50):
+    return FrozenProblemInfo(False, full_score, run_penalty, current_penalty)
+
+
+# ---------- TESTING ----------
+
+
+class BaseRecalcTest:
+    @pytest.fixture(autouse=True)
+    def create_row(self):
+        self._row = StandingsRow(1, 'user', [], 0, 0, False)
+
+    def recalc_task_score(self, task_score, problem_info):
+        recalc_task_score(self._row, task_score, problem_info)
+
+    def assert_unchanged(self, task_score, problem_info):
+        orig_score = copy(task_score)
+        self.recalc_task_score(task_score, problem_info)
+        assert orig_score.score == task_score.score
+        assert orig_score.status == task_score.status
+
+    def assert_updated(self, task_score, problem_info, expected_score):
+        self.recalc_task_score(task_score, problem_info)
+        assert task_score.score == str(expected_score)
+        assert task_score.status == Status.REVIEW
+
+
+class TestNormal(BaseRecalcTest):
+    # Status.TESTING and past_deadline() - should never happen
+
+    @pytest.mark.parametrize('problem_info_gen', [problem_past_deadline, normal_problem])
+    @pytest.mark.parametrize('task_score_gen', [score_ok, score_review])
+    def test_solved(self, problem_info_gen, task_score_gen):
+        """already solved - shouldn't recalculate"""
+        self.assert_unchanged(task_score_gen(), problem_info_gen())
+
+    @pytest.mark.parametrize('task_score_gen', [score_partial, score_not_submitted])
+    def test_frozen(self, task_score_gen):
+        """will never be solved - shouldn't recalculate"""
+        self.assert_unchanged(task_score_gen(), problem_past_deadline())
+
+    def test_partial(self):
+        task_score = score_partial()
+        problem_info = normal_problem(full_score=100, run_penalty=10, current_penalty=50)
+        self.assert_updated(task_score, problem_info, 40)
+
+    def test_not_submitted(self):
+        task_score = score_not_submitted()
+        problem_info = normal_problem(full_score=100, current_penalty=50)
+        self.assert_updated(task_score, problem_info, 50)
+
+    @pytest.mark.parametrize('has_partial', [False, True])
+    def test_testing(self, has_partial):
+        task_score = score_testing(has_partial)
+        problem_info = normal_problem(full_score=100, run_penalty=10, current_penalty=50)
+        if has_partial:
+            self.assert_updated(task_score, problem_info, 40)
+        else:
+            self.assert_updated(task_score, problem_info, 50)
+
+    @pytest.mark.parametrize('problem_info_gen', [problem_past_deadline, normal_problem])
+    @pytest.mark.parametrize('has_partial', [False, True])
+    def test_rejected(self, problem_info_gen, has_partial):
+        task_score = score_rejected(has_partial)
+        problem_info = problem_info_gen(full_score=100, run_penalty=10)
+        if has_partial:
+            self.assert_updated(task_score, problem_info, 90)
+        else:
+            self.assert_updated(task_score, problem_info, 100)
+        self.recalc_task_score(task_score, problem_info)
+
+
+class TestKr(BaseRecalcTest):
+    class MockConfig:
+        class options:
+            max_kr = None
+
+    @pytest.fixture(autouse=True)
+    def mock_config(self, monkeypatch):
+        monkeypatch.setattr('kks.cmd.top.Config', TestKr.MockConfig)
+
+    def test_testing_maxkr(self):
+        TestKr.MockConfig.options.max_kr = True
+        task_score = score_testing(is_kr=True)
+        self.assert_updated(task_score, problem_past_deadline(), 200)
+
+    def test_testing_nomaxkr(self):
+        TestKr.MockConfig.options.max_kr = False
+        task_score = score_testing(is_kr=True)
+        self.assert_unchanged(task_score, problem_past_deadline())
