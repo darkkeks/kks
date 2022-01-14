a23dd30739a2c9f474d99f7247cf50e3578b879e
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2022-01-14 20:41:27 +0300

    Submissions: update DB import script

--- scripts/new_submissions/utils/db-import.py
+++ scripts/new_submissions/utils/db-import.py
@@ -3,7 +3,13 @@ import json
 import re
 from getpass import getpass
 from pathlib import Path
-from db import BotDB
+
+import click
+from kks.ejudge import ejudge_submissions_judge, ejudge_users_judge
+from kks.util.common import prompt_choice
+from kks.util.ejudge import EjudgeSession
+
+from db import BotDB, import_ej_users
 
 try:
     from telethon.sync import TelegramClient
@@ -34,14 +40,14 @@ def create_db(db_path, user_file, msg_file):
         lines = f.read().split('\n')
 
     for line in lines:
-        m = re.search(r'\[.+?\] (\d+) - .* \[(.+?)\]', line)
-        # TODO add ej_user and problem, import pending submissions
+        m = re.search(r'\[.+?\] (\d+) - .*(?: \[(.+?)\])?', line)
         if not m:
             continue
         sub_id, user = m.groups()
-        if user not in users:
-            continue
-        db.assign_submission(sub_id, users[user][0])
+        if user in users:
+            db.assign_submission(sub_id, users[user][0])
+        else:
+            db._conn.execute('INSERT OR IGNORE INTO submissions(id) VALUES (?)', (sub_id))
     db.commit()
     print(f'Imported messages into {db_path.resolve()}')
 
@@ -71,7 +77,7 @@ def create_dump(user_file, msg_file):
         f.write('\n'.join(msgs))
 
 
-def main():
+def v1_to_v2():
     cwd = Path.cwd().resolve()
     db_path = cwd/'caos.db'
 
@@ -86,6 +92,57 @@ def main():
             print('users.json or msgcat.txt is not found')
             return
     create_db(db_path, user_file, msg_file)
+
+
+def v2_to_v2_1():
+    cwd = Path.cwd().resolve()
+    db_path = cwd/'caos.db'
+    db = BotDB(db_path)
+    db.init()
+    print('Importing ejudge users...', end='', flush=True)
+    session = EjudgeSession()
+    users = ejudge_users_judge(session, show_not_ok=True, show_invisible=True, show_banned=True)
+    import_ej_users(db, users)
+    print('OK')
+    user_map = {u.name: u.id for u in users}
+    print('Adding missing values to submissions...', end='', flush=True)
+    sub_ids = [x[0] for x in db._conn.execute('SELECT id FROM submissions').fetchall()]
+    if not sub_ids:
+        print('No submissions found')
+        return
+    sub_data = []
+    # Filter is passed in the query string.
+    # URLs longer than 2K may not be supported.
+    # ~10 chars for one id -> filter size ~= 1K.
+    BATCH_SIZE = 100
+    for i in range(0, len(sub_ids), BATCH_SIZE):
+        filter_ = '||'.join(f'id=={sub_id}' for sub_id in sub_ids[i:i+BATCH_SIZE])
+        sub_data += [
+            (user_map.get(sub.user), sub.problem, sub.id)
+            for sub in ejudge_submissions_judge(session, filter_, last_run=0)
+        ]
+    import pickle
+    with open('dump', 'wb') as f:
+        pickle.dump(sub_data, f)
+    db._conn.executemany('UPDATE submissions SET (user, problem) = (?, ?) WHERE id = ?', sub_data)
+    db.commit()
+    print('OK')
+
+
+def main():
+    choices = [
+        'Import old messages (from bot V1)',
+        'Add user and problem info for old submissions (from bot V2.0)',
+        click.style('Cancel', fg='red')
+    ]
+    choice = prompt_choice('Select action', choices)
+    if choice == len(choices) - 1:
+        print('Cancelled')
+        return
+    if choice == 0:
+        return v1_to_v2()
+    elif choice == 1:
+        return v2_to_v2_1()
 
 
 if __name__ == '__main__':
