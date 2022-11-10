0d326ecf4e512545d681a62a02d470a89525fce9
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2022-11-10 22:30:25 +0300

    Fix bootstrap script

--- scripts/new_submissions/bootstrap.sh
+++ scripts/new_submissions/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker colume create caos-submissions-data 2>/dev/null
+docker volume create caos-submissions-data 2>/dev/null
 docker run -it --rm -v caos-submissions-data:/data caos-submissions-bot kks auth --judge
