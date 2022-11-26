d319b2c2cc73e4b1c7d57955f6bccbc7ae0615de
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2022-11-26 05:43:26 +0300

    Add "kks get -r" from kks-judge; Improve custom click classes

--- kks/cmd/get.py
+++ kks/cmd/get.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 import click
 
-from kks.ejudge import Links
+from kks.ejudge import Links, Page
+from kks.util.click import ArgNotRequiredIf, RequiredIf
 from kks.util.common import format_file
 from kks.util.ejudge import EjudgeSession
 from urllib.parse import parse_qs, urlsplit
@@ -11,24 +12,28 @@ from urllib.parse import parse_qs, urlsplit
 @click.command(short_help='Download a file from ejudge')
 @click.option('-o', '--output',
               type=click.Path(dir_okay=False, writable=True, resolve_path=True),
+              cls=RequiredIf, required_if='run_id',
               help='Output file name')
 @click.option('-f', '--force', is_flag=True, default=False,
               help='Force overwrite existing file')
-@click.argument('url')
-def get(output, force, url):
+@click.option('-r', '--run-id', type=int,
+              help='Download a submission')
+@click.argument('url', cls=ArgNotRequiredIf, not_required_if='run_id')
+def get(output, force, run_id, url):
     """Download a file from ejudge"""
-    parts = urlsplit(url)
-    if parts.netloc != Links.DOMAIN:
-        click.secho(
-            f'Not an ejudge URL. Use wget/curl instead.',
-            fg='red', err=True
-        )
-        return
-    if output is None:
-        query = parse_qs(parts.query)
-        # If file name is included in query, use it.
-        # Otherwise, use the name from url path or domain name.
-        output = query.get('file', [''])[0] or parts.path.rsplit('/', 1)[1] or parts.netloc
+    if run_id is None:
+        parts = urlsplit(url)
+        if parts.netloc != Links.DOMAIN:
+            click.secho(
+                f'Not an ejudge URL. Use wget/curl instead.',
+                fg='red', err=True
+            )
+            return
+        if output is None:
+            query = parse_qs(parts.query)
+            # If file name is included in query, use it.
+            # Otherwise, use the name from url path or domain name.
+            output = query.get('file', [''])[0] or parts.path.rsplit('/', 1)[1] or parts.netloc
 
     out_file = Path(output)
     if out_file.exists() and not force:
@@ -38,7 +43,9 @@ def get(output, force, url):
         )
     out_file.parent.mkdir(parents=True, exist_ok=True)
 
-    if EjudgeSession.needs_auth(url):
+    if run_id is not None:
+        page = EjudgeSession().get_page(Page.DOWNLOAD_SOURCE, {'run_id': run_id})
+    elif EjudgeSession.needs_auth(url):
         page = EjudgeSession().get(url)
     else:
         import requests
--- kks/util/click.py
+++ kks/util/click.py
@@ -10,7 +10,16 @@ class Choice2(click.Choice):
 
 
 # classes to create a flag with optional value, see https://stackoverflow.com/a/44144098
-# click 8.0 should support something like this (https://github.com/pallets/click/issues/549)
+# TODO Replace with click 8.0 options? Example:
+# @click.option(
+#     '--code', is_flag=False, flag_value='_default_',
+#     type=click.Choice(['_default_', 'all', 'rejects'])
+# )
+# Issues:
+# 1. _default_ is shown in help text (can be fixed by using a custom Choice class)
+# 2. Shared help text for option with and without value.
+#    Looks less intuitive than in current implementation. Fix by using custom Command class?
+
 class FlagOption(click.Option):
     """ Mark this option as getting a _opt option """
     is_optflag = True
@@ -74,3 +83,45 @@ class GroupedGroup(click.Group):
                     with formatter.indentation():
                         formatter.write_dl(rows)
                         formatter.write_paragraph()
+
+
+class ArgNotRequiredIf(click.Argument):
+    # Based on https://stackoverflow.com/a/44349292
+    def __init__(self, *args, not_required_if, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.not_required_if = not_required_if
+        assert self.not_required_if, "'not_required_if' parameter required"
+
+    def handle_parse_result(self, ctx, opts, args):
+        self_present = opts.get(self.name) is not None
+        other_present = opts.get(self.not_required_if) is not None
+
+        if other_present:
+            if self_present:
+                raise click.UsageError(
+                    f"Illegal usage: {self.make_metavar()} is mutually exclusive with {self._get_other_opts(ctx)}"
+                )
+            else:
+                self.required = False
+
+        return super().handle_parse_result(ctx, opts, args)
+
+    def _get_other_opts(self, ctx):
+        for param in ctx.command.params:
+            if param.name == self.not_required_if:
+                return ' / '.join([f"'{opt}'" for opt in param.opts])
+
+
+class RequiredIf(click.Option):
+    def __init__(self, *args, required_if, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.required_if = required_if
+        assert self.required_if, "'required_if' parameter required"
+
+    def handle_parse_result(self, ctx, opts, args):
+        other_present = opts.get(self.required_if) is not None
+
+        if other_present:
+            self.required = True
+
+        return super().handle_parse_result(ctx, opts, args)
